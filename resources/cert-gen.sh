51db00c9b5367b457e18980dfcd00bc7762de6ac
Nicolas Mafra <nicolas.mafra@fcamara.com.br>
2020-02-13 21:29:54 -0300

    Add apenas certificado da ca no truststore

---
+++ resources/cert-gen.sh
@@ -0,0 +1,46 @@
+# gera a chave e o certificado da CA
+
+# gera a chave da CA
+openssl genrsa -out cakey.pem 4096
+
+# gera o certificado auto assinado da CA
+openssl req -x509 -new -key cakey.pem -out ca.cer -days 730 -subj '/CN=CA'
+
+
+
+# gera a chave e o certificado do servidor
+
+# gera uma chave para o servidor
+openssl genrsa -out serverkey.pem 2048
+
+# gera um certificado sem assinatura para o servidor
+openssl req -new -key serverkey.pem -out server.csr -subj '/CN=localhost:8080'
+
+# usa a CA para assinar o certificado do servidor
+openssl x509 -req -in server.csr -CA ca.cer -CAkey cakey.pem -CAcreateserial -sha256 -days 365 -out server.cer
+
+# junta o certificado com a chave em um p12
+openssl pkcs12 -export -in server.cer -inkey serverkey.pem -out server.p12 -password pass:serverpass
+
+
+
+# repetição do processo do servidor
+# gera a chave e o certificado do cliente
+
+# gera uma chave para o cliente
+openssl genrsa -out clientkey.pem 2048
+
+# gera um certificado sem assinatura para o cliente
+openssl req -new -key clientkey.pem -out client.csr -subj '/CN=localhost:8081'
+
+# usa a CA para assinar o certificado do cliente
+openssl x509 -req -in client.csr -CA ca.cer -CAkey cakey.pem -CAserial ca.srl -sha256 -days 365 -out client.cer
+
+# junta o certificado com a chave em um p12
+openssl pkcs12 -export -in client.cer -inkey clientkey.pem -out client.p12 -password pass:clientpass
+
+
+
+# registra o certificado da CA como confiável
+rm truststore.jks
+keytool -import -keystore truststore.jks -storepass password -file ca.cer -noprompt
